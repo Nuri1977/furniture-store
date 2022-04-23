c2a1685922405a2c11bcd495e539b071a3206b91
Nuri1977 <nurilacka@gmail.com>
2022-04-23 11:20:49 +0200

    Add showroom controller and index view

---
+++ app/controllers/showrooms_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class ShowroomsController < ApplicationController
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'pages#home'
+  get '/showrooms', to: 'showrooms#index'
 end
