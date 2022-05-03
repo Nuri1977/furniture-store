0cb14c3ecd5bfeb676dc74fb49746d760dacf469
Nuri1977 <nurilacka@gmail.com>
2022-05-03 16:13:17 +0200

    Change names of showrooms phones in views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 Showroom.create!(
   name: 'Struga',
   adress: 'Zufer Music',
-  phone: 51561515516615,
-  mobile: 464646464,
+  phone: +389 02 2356-152,
+  mobile: +389 71 241-526,
   email: 'nurilacka@gmail.com',
   image_url: 'http://www.mebel-lacka.com/img/struga.jpg',
   geolocation: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2443.8433730110883!2d20.682921319041682!3d41.17828199999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1350e715839c91f9%3A0xb5730a939ef9aa17!2sLacka!5e1!3m2!1sen!2smk!4v1651303413089!5m2!1sen!2smk'
@@ -11,8 +11,8 @@ Showroom.create!(
 Showroom.create!(
   name: 'Skopje',
   adress: 'Industriska zona Vizbegovo',
-  phone: 51561515516615,
-  mobile: 464646464,
+  phone: +389 02 2356-152,
+  mobile: +389 71 241-526,
   email: 'nurilacka@gmail.com',
   image_url: 'http://www.mebel-lacka.com/img/skopje.jpg',
   geolocation: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d617439.6126767815!2d20.844601377995975!3d42.027950200000014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x135414ec3a44a5bd%3A0x6ec10a740eaf4bb9!2sLa%C4%8Dka%20Mebel%20Skopje!5e1!3m2!1sen!2smk!4v1651304342741!5m2!1sen!2smk'
@@ -21,8 +21,8 @@ Showroom.create!(
 Showroom.create!(
   name: 'Gostivar',
   adress: 'Industriska Zona',
-  phone: 51561515516615,
-  mobile: 464646464,
+  phone: +389 02 2356-152,
+  mobile: +389 71 241-526,
   email: 'nurilacka@gmail.com',
   image_url: 'http://www.mebel-lacka.com/img/gostivar.jpg',
   geolocation: 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2443.8373285498014!2d20.685132!3d41.178444!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1351579ae72358db%3A0x48381bde365dedb7!2sLACKA!5e1!3m2!1sen!2smk!4v1651302929145!5m2!1sen!2smk'
