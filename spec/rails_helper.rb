cec70263c91ffcd7b5ddf35bb142c7f214a64098
Nuri1977 <nurilacka@gmail.com>
2022-05-14 10:59:31 +0200

    Fix specs for showroom controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,9 @@ require 'rspec/rails'
 require 'support/factory_bot'
 require 'support/shoulda_matchers'
 # Add additional requires below this line. Rails is not loaded until this point!
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
