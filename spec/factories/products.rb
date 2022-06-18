c626126d11feb737af2756832de6fb2de809e910
Nuri1977 <nurilacka@gmail.com>
2022-06-18 09:48:57 +0200

    Fix rubocop

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :product do
     association :subcategory
     product_name { Faker::House.unique.room }
-    price { Faker::Number.number(digits: 3)}
+    price { Faker::Number.number(digits: 3) }
     dimensions { 'MyText' }
   end
 end
