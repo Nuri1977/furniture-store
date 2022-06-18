71c44de8f50cd4f61f910f9429cfb6d13e95d7fa
Nuri1977 <nurilacka@gmail.com>
2022-06-18 10:39:06 +0200

    remove category controller actions from UI

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,59 +1,15 @@
 # frozen_string_literal: true
 
 class CategoriesController < ApplicationController
-  before_action :set_category, only: %i[show edit update destroy]
-
-  # GET /categories
-  def index
-    @categories = Category.all
-  end
+  before_action :set_category, only: %i[show]
 
   # GET /categories/1
   def show; end
-
-  # GET /categories/new
-  def new
-    @category = Category.new
-  end
-
-  # GET /categories/1/edit
-  def edit; end
-
-  # POST /categories
-  def create
-    @category = Category.new(category_params)
-
-    if @category.save
-      redirect_to @category
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /categories/1
-  def update
-    if @category.update(category_params)
-      redirect_to @category
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /categories/1
-  def destroy
-    @category.destroy
-    redirect_to categories_url
-  end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_category
     @category = Category.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def category_params
-    params.require(:category).permit(:category_name)
   end
 end
