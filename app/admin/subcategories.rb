7fefa1d3a0ae1de00604e766e530a08d27ce9e0e
Nuri1977 <nurilacka@gmail.com>
2022-06-18 09:55:22 +0200

    Create active admin reasorce for Subcategory model

---
+++ app/admin/subcategories.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Subcategory do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :category_id, :name
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:category_id, :name]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
