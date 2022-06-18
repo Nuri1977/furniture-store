f7a8170d8364b92dad3a42be7af6301cd10bfac0
Nuri1977 <nurilacka@gmail.com>
2022-06-18 11:28:11 +0200

    Add active storage images for categories and subcategories

--- app/admin/subcategories.rb
+++ app/admin/subcategories.rb
@@ -6,7 +6,7 @@ ActiveAdmin.register Subcategory do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :category_id, :name
+  permit_params :category_id, :name, :image
   #
   # or
   #
@@ -15,4 +15,12 @@ ActiveAdmin.register Subcategory do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :image, as: :file
+    end
+    f.actions
+  end
+
 end
--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -3,6 +3,7 @@
 class Subcategory < ApplicationRecord
   belongs_to :category
   has_many :products, dependent: :destroy
+  has_one_attached :image
 
   validates :name, presence: true, length: { maximum: 250 }
 end
