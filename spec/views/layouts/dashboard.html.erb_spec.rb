fb9bf3d5d3b3a3d123e4abfaf68fe34f1b1d053f
Nuri1977 <nurilacka@gmail.com>
2022-06-24 11:01:53 +0200

    Add dahboard spec

---
+++ spec/views/layouts/dashboard.html.erb_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'layouts/dashboard' do
+  let(:user) { create(:user) }
+
+  it 'renders dashboard' do
+    render
+
+    expect(rendered).to include("Welcome back")
+    expect(rendered).to have_link('Account')
+  end
+
+end
