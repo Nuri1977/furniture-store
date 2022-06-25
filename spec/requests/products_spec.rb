00fcd7682f278f574bdcc3df6e5f499e2b8c318b
Nuri1977 <nurilacka@gmail.com>
2022-06-25 10:44:03 +0200

    Update test wir factory

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -4,10 +4,8 @@ require 'rails_helper'
 
 RSpec.describe 'Products', type: :request do
   describe 'GET /show' do
-    category = Category.create(category_name: 'Test category')
-    subcategory = Subcategory.create(name: 'Test subcategory', category_id: category.id)
-    product = Product.create(product_name: 'Test product', price: 100, show_price: true, new_product: true, on_promotion: true, is_outlet: true,
-                             dimensions: 'Test dimensions', subcategory_id: subcategory.id)
+    let!(:product) { create :product }
+
     it 'returns http success' do
       get product_url(product)
       expect(response).to have_http_status(:success)
