893be623724ded24b40e4f02837915e3be09226d
Nuri1977 <nurilacka@gmail.com>
2022-06-25 12:00:21 +0200

    Add javascript code for hamburger menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,21 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+window.addEventListener('load', () => {
+  const hamburgerBtn = document.getElementById('hamburger-btn');
+  const menu = document.getElementById('mobile-menu');
+  const menu1 = document.getElementById('mobile-menu1');
+  const backdrop = document.getElementById('backdrop');
+  hamburgerBtn.addEventListener('click', () => {
+    menu.classList.remove('-translate-x-full');
+    menu1.classList.remove('-translate-x-full');
+    backdrop.classList.remove('hidden');
+  });
+
+  backdrop.addEventListener('click', () => {
+    menu.classList.add('-translate-x-full');
+    menu1.classList.add('-translate-x-full');
+    backdrop.classList.add('hidden');
+  });
+});
