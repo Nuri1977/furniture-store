8c84bb822e2f00610d22cb5f9f87e2fa1def272a
Nuri1977 <nurilacka@gmail.com>
2022-06-30 19:59:46 +0200

    Add cart counter in navigation menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+  def cart_count_over_one
+    @cart.line_items.map(&:quantity).sum
+  end
+
+  def cart_has_items
+    @cart.line_items.count > 0 if @cart
+  end
 end
