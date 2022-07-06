7bc0dab620a9daffbaac02a9bf0cabc1f2b4e177
Nuri1977 <nurilacka@gmail.com>
2022-07-06 18:02:06 +0200

    Clean cart controller actions

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -14,17 +14,6 @@ class CartsController < ApplicationController
   # POST /carts
   # POST /carts.json
   def create
-    @cart = Cart.new(cart_params)
-
-    respond_to do |format|
-      if @cart.save
-        format.html { redirect_to @cart, notice: 'Cart was successfully created.' }
-        format.json { render :show, status: :created, location: @cart }
-      else
-        format.html { render :new }
-        format.json { render json: @cart.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # DELETE /carts/1
@@ -41,16 +30,6 @@ class CartsController < ApplicationController
   # rubocop:enable Rails/I18nLocaleTexts
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_cart
-    @cart = Cart.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def cart_params
-    params.fetch(:cart, {})
-  end
 
   # rubocop:disable Rails/I18nLocaleTexts
   def invalid_cart
